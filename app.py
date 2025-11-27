935d7eaa1f720c4c2a7f6e11355eed97f959329b
Training YWK <leisure.mandy@gmail.com>
2025-11-27 16:47:25 +0700

    test workflow

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+from datetime import datetime
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', message="Heyyy, Flask!")
+
+@app.route('/showdate')
+def showdate():
+    curr_datetime = datetime.now()
+    return render_template('index.html', message="the time now is :", curr_date=curr_datetime)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
